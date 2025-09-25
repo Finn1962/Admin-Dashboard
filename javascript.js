57df5c68465b3a3b8b2a13458223555b2c907beb
Finn Schmidt <finn.schmidt@brainworxx.de>
2025-09-25 11:34:25 +0200

    Layout und Resizing funktioniert ohne Probleme

--- javascript.js
+++ javascript.js
@@ -8,26 +8,43 @@ const kopfleiste = document.getElementById("kopfleiste");
 const kopfleisteResizer = document.getElementById("kopfleisteResizer");
 
 
-document.addEventListener("DOMContentLoaded",()=> {navigationsleisteResizer.style.left = navigationsleiste.offsetWidth + "px"
-                                                    kopfleisteResizer.style.top = kopfleiste.offsetHeight + "px"});
-window.addEventListener("resize",()=> {navigationsleisteResizer.style.left = navigationsleiste.offsetWidth + "px"
-                                        kopfleisteResizer.style.top = kopfleiste.offsetHeight + "px"});
+document.addEventListener("DOMContentLoaded",()=> {
+    navigationsleisteResizer.style.left = navigationsleiste.offsetWidth + "px";
+    navigationsleisteResizer.style.height = (document.body.offsetHeight - kopfleiste.offsetHeight) + "px";
+    kopfleisteResizer.style.top = kopfleiste.offsetHeight + "px";
+});
+window.addEventListener("resize",()=> {
+    navigationsleisteResizer.style.left = navigationsleiste.offsetWidth + "px";
+    navigationsleisteResizer.style.height = (document.body.offsetHeight - kopfleiste.offsetHeight) + "px";
+    kopfleisteResizer.style.top = kopfleiste.offsetHeight + "px";
+});
 
+navigationsleisteResizer.addEventListener("mouseenter", ()=> {
+    mouseoverNavResizer = true
+    mouseoverKopfResizer = false
+});
+kopfleisteResizer.addEventListener("mouseenter", ()=> {
+    mouseoverKopfResizer = true
+    mouseoverNavResizer = false
+});
 document.addEventListener("mousedown", ()=> mousedown = true);
-navigationsleisteResizer.addEventListener("mouseenter", ()=> mouseoverNavResizer = true);
-kopfleiste.addEventListener("mouseenter", ()=> mouseoverKopfResizer = true);
-document.addEventListener("mouseup", ()=> {mousedown = false, mouseoverNavResizer = false, mouseoverKopfResizer = false});
+document.addEventListener("mouseup", ()=> {
+    mousedown = false; 
+    mouseoverNavResizer = false; 
+    mouseoverKopfResizer = false
+});
 
 document.addEventListener("mousemove",(ereigniss)=>{ 
-    if (!mousedown || !mouseoverNavResizer && !mouseoverKopfResizer) return;
+    if (!mousedown || (!mouseoverNavResizer && !mouseoverKopfResizer)) return;
     if (mouseoverNavResizer) {
         const neueWeite = ereigniss.clientX + "px";
         navigationsleiste.style.width = neueWeite;
         navigationsleisteResizer.style.left = neueWeite;
-    }
+    } 
     if (mouseoverKopfResizer) {
         const neueWeite = ereigniss.clientY + "px";
         kopfleiste.style.height = neueWeite;
         kopfleisteResizer.style.top = neueWeite;
+        navigationsleisteResizer.style.height = (document.body.offsetHeight - kopfleiste.offsetHeight) + "px";
     }
 });
